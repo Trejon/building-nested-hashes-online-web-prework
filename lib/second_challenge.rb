a279c6183e667b7f41f902fe35128d97335a3681
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-09 17:36:45 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matricarch => {},
-      :hero => {},
-      :hero_friends => []
+      patriarch: {},
+      matricarch: {},
+      hero: {},
+      hero_friends: []
       }, 
     :capulet => {
-      :patriarch => {},
-      :matricarch => {},
-      :heroine => {},
-      :hero_friends => []
+      patriarch: {},
+      matricarch: {},
+      heroine: {},
+      hero_friends: []
       }
   }
 end
