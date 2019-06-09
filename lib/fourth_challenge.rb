c2e25e5bece02cb8fbd3c887577646ceb3d8e671
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-09 17:45:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,6 +8,7 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Mercutio", age: "18", attidue: "hot-headed"}
       ]
+   }
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
